8d674559f164ca925dfb58925abdc97490015524
Harlan Phillips <133736716+Harlan-Phillips@users.noreply.github.com>
2024-04-04 22:54:46 -0700

    Update studenttypes.py

--- src/studenttypes.py
+++ src/studenttypes.py
@@ -1,1 +1,34 @@
+#create Student Class
+class Student:
+    def __init__(self, barcode, id, email, student_class, instructor, name, role, department, institution, service, caseName):
+        self.barcode = barcode
+        self.id = id
+        self.time_in = time_in
+        self.date = date
+        self.email = email
+        self.student_class = student_class
+        self.instructor = instructor
+        self.name = name
+        self.role = role
+        self.department = department
+        self.institution = institution
+        self.service = service
+        self.caseName = caseName
+
+#create check-in Student Class
+class Student_check:
+    def __init__(self, barcode, id, time_in, date, email, student_class, instructor, name, role, department, institution, service, caseName):
+        self.barcode = barcode
+        self.id = id
+        self.time_in = time_in
+        self.date = date
+        self.email = email
+        self.student_class = student_class
+        self.instructor = instructor
+        self.name = name
+        self.role = role
+        self.department = department
+        self.institution = institution
+        self.service = service
+        self.caseName = caseName
 
