f032eebf305d3f96cb079495cbd62ea494789920
Austin-Chan <157552108+Acnchan8@users.noreply.github.com>
2024-05-14 17:01:12 -0700

    Update studenttypes.py

--- src/studenttypes.py
+++ src/studenttypes.py
@@ -3,8 +3,6 @@ class Student:
     def __init__(self, barcode, id, email, student_class, instructor, name, role, department, institution, service, caseName):
         self.barcode = barcode
         self.id = id
-        self.time_in = time_in
-        self.date = date
         self.email = email
         self.student_class = student_class
         self.instructor = instructor
